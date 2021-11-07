d4b5ebf20e72fbf6180f32c7ff00add44d0264c4
Naoya Yamashita <conao3@gmail.com>
2021-11-07 13:57:55 +0900

    add chapter-a sample app

--- chapter-a/src/App.tsx
+++ chapter-a/src/App.tsx
@@ -1,7 +1,59 @@
-export const App = () => {
+import { ChangeEvent, FC, useState } from "react";
+import styled from "styled-components";
+
+export const App: FC = () => {
+  const [text, setText] = useState<string>("");
+  const [memos, setMemos] = useState<string[]>([]);
+
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const onClickAdd = () => {
+    const newMemos = [...memos, text];
+    setMemos(newMemos);
+    setText("");
+  };
+
+  const onClickDelete = (index: number) => {
+    const newMemos = [...memos];
+    newMemos.splice(index, 1);
+    setMemos(newMemos);
+  };
+
   return (
     <div>
       <h1>Memo app</h1>
+      <input type="text" value={text} onChange={onChangeText} />
+      <SButton onClick={onClickAdd}>Add</SButton>
+      <SContainer>
+        <p>List of memos</p>
+        <ul>
+          {memos.map((memo, index) => (
+            <li key={memo}>
+              <SMemoWrapper>
+                <span>{memo}</span>
+                <SButton onClick={() => onClickDelete(index)}>Delete</SButton>
+              </SMemoWrapper>
+            </li>
+          ))}
+        </ul>
+      </SContainer>
     </div>
   );
 };
+
+const SButton = styled.button`
+  margin-left: 16px;
+`;
+
+const SContainer = styled.div`
+  border: solid 1px #ccc;
+  padding: 16px;
+  margin: 8px;
+`;
+
+const SMemoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
