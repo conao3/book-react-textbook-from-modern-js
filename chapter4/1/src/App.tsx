264473c2100576fd69b71bd50d0d8c852d291347
Naoya Yamashita <conao3@gmail.com>
2021-11-07 12:34:32 +0900

    add onClickButton callback function

--- chapter4/1/src/App.tsx
+++ chapter4/1/src/App.tsx
@@ -1,8 +1,12 @@
 export const App = () => {
+    const onClickButton = () => {
+        console.log('Button clicked');
+    };
     return (
       <div>
         <h1>Hello World</h1>
         <p>Hi React!</p>
+        <button onClick={onClickButton}> Click me!</button>
       </div>
     );
   };
