e48b67829abc80a4cb87dc716516a4546d9e8af2
Naoya Yamashita <conao3@gmail.com>
2021-11-07 12:38:32 +0900

    use CSS-in-JS

--- chapter4/1/src/App.tsx
+++ chapter4/1/src/App.tsx
@@ -2,10 +2,16 @@ export const App = () => {
     const onClickButton = () => {
         console.log('Button clicked');
     };
+
+    const contentStyle = {
+        color: "blue",
+        fontSize: "20px",
+    }
+
     return (
       <div>
-        <h1>Hello World</h1>
-        <p>Hi React!</p>
+        <h1 style={{ color: "red" }}>Hello World</h1>
+        <p style={contentStyle}>Hi React!</p>
         <button onClick={onClickButton}> Click me!</button>
       </div>
     );
