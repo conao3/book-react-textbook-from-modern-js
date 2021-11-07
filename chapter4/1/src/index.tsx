21ce9149c05ab14caf48997effe6d5c6a5f377ab
Naoya Yamashita <conao3@gmail.com>
2021-11-07 12:29:37 +0900

    first react step

---
+++ chapter4/1/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+    <h1>Hello World</h1>
+    <p>Hi React!</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
